e61df2d74575433cdbf48fef965896fe178b5ffc
younghoonKimm <choosey_@naver.com>
2023-01-28 21:18:28 +0900

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,28 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { useState } from "react";
 import { CookiesProvider } from "react-cookie";
+import { RecoilRoot } from "recoil";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <SessionProvider session={session}>
-      <CookiesProvider>
-        <Component {...pageProps} />
-      </CookiesProvider>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <CookiesProvider>
+            <Component {...pageProps} />
+          </CookiesProvider>
+        </RecoilRoot>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
